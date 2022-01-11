6d793a5c57fbdd3db29b8f9d17ca4ec5ba5ab6ce
Mark Outeke <64286169+Mark-Outeke@users.noreply.github.com>
2022-01-11 11:54:30 +0300

    Update animals.py

--- animals.py
+++ animals.py
@@ -1,5 +1,7 @@
+#What does this code do?
 animals = ["lion", "cat", "leopard", "jaguar", "tiger"]
 for animal in animals:
     print(animal.title())
     print("A" + " " + animal.title() + " " + "would make such a nice pet.")
 print("\nAll these animals belong to the cat family!")
+#Show results for this code.
